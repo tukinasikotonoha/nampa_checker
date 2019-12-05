fb022a4e9a997cde8b624bd7e08c1e94fe5022c4
yuikotonoha <harutinkudo042@gmail.com>
2019-12-05 13:00:31 +0900

    [add]sorceryログインで使うusersテーブルを作成

--- db/migrate/20191205034356_sorcery_core.rb
+++ db/migrate/20191205034356_sorcery_core.rb
@@ -1,13 +1,12 @@
 class SorceryCore < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :email,            null: false
+      t.string :email
       t.string :crypted_password
       t.string :salt
-
-      t.timestamps                null: false
+      t.string :name, null: false
+      t.integer :role, null: false, default: 0
+      t.timestamps null: false
     end
-
-    add_index :users, :email, unique: true
   end
 end
