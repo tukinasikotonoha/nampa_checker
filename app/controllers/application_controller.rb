5ff7ce958196a9a005f66ef57113387a3ef32083
yuikotonoha <harutinkudo042@gmail.com>
2019-12-05 14:13:40 +0900

    [modify]ログインしていない場合に特定のアクションを実行出来ないように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  # ログイン済みユーザーかどうか確認
+  before_action :require_login
+
+  private
+  def not_authenticated
+    redirect_to login_path, alert: "ログインしてください"
+  end
 end
