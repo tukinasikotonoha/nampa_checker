bd397eae9f84213f66009b8b3c6694b6a2170211
yuikotonoha <harutinkudo042@gmail.com>
2019-12-05 14:03:33 +0900

    [modify]userモデルにnameとroleとバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :role, presence: true
+  # ユーザーが管理者かどうか判定
+  enum role: { general: 0, admin: 1 }
 end
