725bc4b12da686d1268e9861c3cc8171c2a8aed8
yuikotonoha <harutinkudo042@gmail.com>
2019-12-05 18:39:23 +0900

    [modify]ロケットハッシュを修正

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   post '/login', to: 'user_sessions#create'
   delete '/logout', to: 'user_sessions#destroy'
   # Twitterログイン
-  post 'oauth/callback' => 'oauths#callback'
-  get 'oauth/callback' => 'oauths#callback' # for use with Github, Facebook
-  get 'oauth/:provider' => 'oauths#oauth', :as => :auth_at_provider
+  post 'oauth/callback', to: 'oauths#callback'
+  get 'oauth/callback', to: 'oauths#callback' # for use with Github, Facebook
+  get 'oauth/:provider', to: 'oauths#oauth', as: :auth_at_provider
 end
