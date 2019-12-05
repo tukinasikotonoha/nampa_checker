3a398cc8059d4570cd9558955d8a8f62fb4ea93e
yuikotonoha <harutinkudo042@gmail.com>
2019-12-05 14:47:06 +0900

    [modify]ログインとログアウトボタンのリンクを修正、topsコントローラーでrequire_loginを無効化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
 
   private
   def not_authenticated
-    redirect_to login_path, alert: "ログインしてください"
+    redirect_to root_path, alert: "ログインしてください"
   end
 end
--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,2 +1,4 @@
 class TopsController < ApplicationController
+  skip_before_action :require_login, only: %i[index]
+  def index; end
 end
