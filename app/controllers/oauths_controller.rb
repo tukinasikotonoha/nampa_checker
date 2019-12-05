6b8e06bb51e0367a843775fac7b7170849f6326c
yuikotonoha <harutinkudo042@gmail.com>
2019-12-05 14:53:04 +0900

    [modify]twitter認証をキャンセルした場合のリダイレクト先を修正

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -9,7 +9,7 @@ class OauthsController < ApplicationController
 
   def callback
     # Twitter認証をキャンセルした場合401が返ってくるのでリダイレクトさせる
-    redirect_to login_url and return if params[:denied].present?
+    redirect_to root_path and return if params[:denied].present?
     provider = "twitter"
     @user = login_from(provider)
     if @user
@@ -18,7 +18,6 @@ class OauthsController < ApplicationController
       begin
         @user = create_from(provider)
         # NOTE: this is the place to add '@user.activate!' if you are using user_activation submodule
-
         reset_session # protect from session fixation attack
         auto_login(@user)
         redirect_to root_path, :notice => "#{provider.titleize}でログインしました!"
