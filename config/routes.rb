1ae82a512e4c0412e067c40f77485c98023dea2e
yuikotonoha <harutinkudo042@gmail.com>
2019-12-05 14:10:45 +0900

    [add]usersのルーティングと、ログイン・ログアウトのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tops#index'
+  resources :users, only: %i[new create]
+  # ログイン機能
+  get '/login', to: 'user_sessions#new'
+  post '/login', to: 'user_sessions#create'
+  delete '/logout', to: 'user_sessions#destroy'
 end
