08759a02877e2780e114ce6458b3a791cd5c84de
yuikotonoha <harutinkudo042@gmail.com>
2019-12-06 17:46:56 +0900

    [delete]使っていないアクション・ルーティングを削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,6 @@
 class UsersController < ApplicationController
-  skip_before_action :require_login, only: %i[new create]
+  skip_before_action :require_login, only: %i[show]
 
-  def new
-    @user = User.new
-  end
+  def show; end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to root_path, success: 'ユーザーを作成しました'
-    else
-      redirect_to root_path, danger: 'ユーザーの作成に失敗しました'
-    end
-  end
-
-  private
-
-  # Strong Parameter
-  def user_params
-    params.require(:user).permit(
-      :name
-    )
-  end
 end
