d5cc0fd6be93851d75388cc8ca162df8285b1fb0
yuikotonoha <harutinkudo042@gmail.com>
2019-12-07 13:55:41 +0900

    [modify]マイページの検証日時の表示を修正、検証結果を降順に変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   skip_before_action :require_login, only: %i[show]
 
   def show
-    @results = current_user.results
+    @results = current_user.results.order(created_at: "DESC")
   end
 end
