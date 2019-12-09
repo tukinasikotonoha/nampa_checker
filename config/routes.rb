0651f6b95b32679151c8e7bf4c8270512675508c
yuikotonoha <harutinkudo042@gmail.com>
2019-12-09 13:39:02 +0900

    [modify]コメントを行の後ろに移動

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tops#index'
   resources :results, param: :uuid, only: %i[index show new create destroy]
-  # マイページ
-  get '/mypage', to: 'users#show'
-  # ログアウト機能
-  delete '/logout', to: 'user_sessions#destroy'
+  get '/mypage', to: 'users#show' # マイページ
+  delete '/logout', to: 'user_sessions#destroy' # ログアウト機能
   post 'oauth/callback', to: 'oauths#callback' # Twitterログイン
   get 'oauth/callback', to: 'oauths#callback' # for use with Github, Facebook
   get 'oauth/:provider', to: 'oauths#oauth', as: :auth_at_provider
