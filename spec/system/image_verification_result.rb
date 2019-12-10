10e71c8ba296cfb739d27cf858f51f2008b1c247
yuikotonoha <harutinkudo042@gmail.com>
2019-12-10 16:49:11 +0900

    [add]10メガバイトを超える画像を添付してアップロードした場合のテストを追加

--- spec/system/image_verification_result.rb
+++ spec/system/image_verification_result.rb
@@ -8,8 +8,10 @@ RSpec.describe 'Results', type: :system do
   describe 'TOPページ' do
     context '10メガバイトを超える画像を添付してアップロード' do
       it '「画像のファイルサイズは10メガバイト以下でお願いします」とフラッシュメッセージが表示される' do
+        attach_file 'result[image]', "#{Rails.root}/spec/system/images/images_over_10_megabytes.jpg"
+        click_button '女性に見える確率を検証'
+        expect(page).to have_content '画像のファイルサイズは10メガバイト以下でお願いします'
         binding.pry
-        visit root_path
       end
     end
   end
