c1d51114858f2f32a37612330a9de341e00cebea
yuikotonoha <harutinkudo042@gmail.com>
2019-12-12 11:28:44 +0900

    [add]AdminLTE のインストール,admin.scssとadmin.jsを追加、管理画面へのログインルーティング追加

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,5 @@
+//= require rails-ujs
+//= require jquery/dist/jquery.min
+//= require bootstrap/dist/js/bootstrap.min
+//= require admin-lte/dist/js/adminlte.min
+//= require board_image_preview
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,13 @@ Rails.application.routes.draw do
   post 'oauth/callback', to: 'oauths#callback' # Twitterログイン
   get 'oauth/callback', to: 'oauths#callback' # for use with Github, Facebook
   get 'oauth/:provider', to: 'oauths#oauth', as: :auth_at_provider
+  # 管理者ログイン、管理者画面、管理者画面ダッシュボード
+  namespace :admin do
+    get '/login', to: 'user_sessions#admin_login'
+    post '/login', to: 'user_sessions#create'
+    delete '/logout', to: 'user_sessions#admin_logout'
+    get '/', to: 'dashboards#index'
+  end
   # テスト環境でTwitterログイン認証を通すためのルーティング
   if Rails.env.test?
     get '/login_as/:user_id', to: 'test/sessions#login_as'
