79f4c154b0f4518b9e86b5ee5950f01b4208800d
yuikotonoha <harutinkudo042@gmail.com>
2019-12-12 21:12:59 +0900

    [add]管理者画面へのログイン系テストを作成、テストファイルの末尾に_specを付与

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,14 @@
 FactoryBot.define do
   factory :user do
+    password { 'password' }
+    password_confirmation { 'password' }
     sequence(:name) { |n| "user#{n}" }
     role { :general }
+
+    trait :admin do
+      email { 'admin@example.com' }
+      sequence(:name) { |n| "admin#{n}" }
+      role { :admin }
+    end
   end
 end
