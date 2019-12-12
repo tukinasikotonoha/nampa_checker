0f8ba2f3fcef1e357af036e6a3aa670c6ed02221
yuikotonoha <harutinkudo042@gmail.com>
2019-12-12 11:33:11 +0900

    [add]AdminControllerを追加

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,14 @@
+class AdminController < ApplicationController
+  before_action :admin_user?
+
+  def admin_user?
+    redirect_back_or_to root_path, warning: '権限がありません' unless current_user.admin?
+  end
+
+  private
+
+  def not_authenticated
+    flash[:warning] = 'ログインしてください'
+    redirect_to admin_login_path
+  end
+end
