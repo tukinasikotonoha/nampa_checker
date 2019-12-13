831ad8ec39ab2328c0385641b4bbcfb4807bcc04
yuikotonoha <harutinkudo042@gmail.com>
2019-12-13 16:15:10 +0900

    [modify]シンボルで指定する書き方に修正

--- spec/factories/results.rb
+++ spec/factories/results.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :result do
     image { Rack::Test::UploadedFile.new("#{Rails.root}/spec/system/images/female1.jpg", 'image/png') }
-    gender { 'female' }
+    gender { :female }
     score { 80 }
     user
 
