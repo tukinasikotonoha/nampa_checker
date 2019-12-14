57cf8cf8a4249b8ceca36d13a917b12b9ea6c75b
yuikotonoha <harutinkudo042@gmail.com>
2019-12-14 15:53:54 +0900

    [modify]full_titleメソッド内でif adminの返り値を修正、キーワード引数を使う書き方に修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   # ページごとの完全なタイトルを返します。
-  def full_title(page_title = '', admin)
-    if admin == true
-      base_title = 'ナンパされチェッカー(管理画面)'
-    else
-      base_title = 'ナンパされチェッカー'
-    end
+  def full_title(page_title: '', admin: true)
+    base_title = if admin
+                   'ナンパされチェッカー(管理画面)'
+                 else
+                   'ナンパされチェッカー'
+                 end
     if page_title.empty?
       base_title
     else
