63775416088ce41c6899c7262b41e74b03989433
yuikotonoha <harutinkudo042@gmail.com>
2019-12-15 12:46:12 +0900

    [add]ローカル環境で500のエラーページを見せたい場合の記述を追記

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,9 +10,9 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
-  # config.consider_all_requests_local = true
+  config.consider_all_requests_local = true
   # ローカル環境で500のエラーページを見せたい時は下記のfalseのコメントアウトを外す
-  config.consider_all_requests_local = false
+  # config.consider_all_requests_local = false
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
