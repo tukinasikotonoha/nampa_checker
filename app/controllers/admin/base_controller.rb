4d781c64c244eec7fa5e527e10b433bf770b80c5
yuikotonoha <harutinkudo042@gmail.com>
2019-12-16 14:14:40 +0900

    [modify]メソッド名をわかりやすくする為再修正(access_prohibit_non_admin_user → prohibit_access_except_admin_user）

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,8 +1,8 @@
 class Admin::BaseController < ApplicationController
-  before_action :access_prohibit_non_admin_user
+  before_action :prohibit_access_except_admin_user
   layout 'admin_dashboards'
 
-  def access_prohibit_non_admin_user
+  def prohibit_access_except_admin_user
     redirect_back_or_to root_path, warning: '権限がありません' unless current_user.admin?
   end
 
