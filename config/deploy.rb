000d53c35a0b65508d51dd84018f00f1544c9dc2
yuikotonoha <harutinkudo042@gmail.com>
2019-12-22 17:13:15 +0900

    [add]capistrano 設定ファイルを更新

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,70 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.11.2"
+# capistranoのバージョン固定
+lock "~> 3.10.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+# デプロイするアプリケーション名
+set :application, 'nampa_checker'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# cloneするgitのレポジトリ
+# 1-3で設定したリモートリポジトリのurl
+set :repo_url, 'git@github.com:yuikotonoha/nampa_checker.git'
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+# deployするブランチ。デフォルトはmasterなのでなくても可。
+set :branch, 'develop'
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+# deploy先のディレクトリ。
+set :deploy_to, '/var/www/nampa_checker'
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
 
+# シンボリックリンクをはるファイル
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
-
+append :linked_files, "config/master.key"
+# シンボリックリンクをはるフォルダ
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system"
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# 保持するバージョンの個数(※後述)
+set :keep_releases, 5
 
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
+# rubyのバージョン
+# rbenvで設定したサーバー側のrubyのバージョン
+set :rbenv_ruby, '2.6.4'
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+# 出力するログのレベル。
+set :log_level, :debug
 
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+# デプロイのタスク
+namespace :deploy do
+
+  # unicornの再起動
+  desc 'Restart application'
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+
+  # データベースの作成
+  desc 'Create database'
+  task :db_create do
+    on roles(:db) do |host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          # データベース作成のsqlセット
+          # データベース名はdatabase.ymlに設定した名前で
+          sql = "CREATE DATABASE IF NOT EXISTS nampa_checker_production;"
+          # クエリの実行。
+          # userとpasswordはmysqlの設定に合わせて
+          execute "mysql --user=root --password=#{Rails.application.credentials.mysql_password} -e '#{sql}'"
+
+        end
+      end
+    end
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+    end
+  end
+end
