128e85a04315998dc599e5d1df7e4ecdf199166f
yuikotonoha <harutinkudo042@gmail.com>
2019-12-25 10:28:44 +0900

    [modify]require_loginのコールバックするアクション修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
-  skip_before_action :require_login, only: %i[show]
-
   def show
     @results = current_user.results.page(params[:page]).order(id: :desc)
   end
