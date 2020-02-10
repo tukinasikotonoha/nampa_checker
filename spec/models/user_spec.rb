309ac98c04ba09bbe41fcf358a530db38eeb72f2
yuikotonoha <harutinkudo042@gmail.com>
2020-02-10 09:33:09 +0900

    [modify]既に作成されたユーザーのletの命名を分かりやすいように「already_user」に修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 	context "メールアドレスが重複したユーザーが作成されようとした時" do
-		let!(:user) { create :user, :email_duplication }
+		let!(:already_user) { create :user, :email_duplication }
 		it "エラーになる" do
 			user = User.new(email: "user@example.com", name: "ことのは", password: "password", password_confirmation: "password")
 			user.valid?
